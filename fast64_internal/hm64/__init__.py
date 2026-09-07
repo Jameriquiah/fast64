906b684eb08ca8fdbca84ba63a2f2fb09c5ec942
Jameriquiah <42100286+Jameriquiah@users.noreply.github.com>
2026-09-07 01:13:04 -0400

    [OOT] Scene Exporter Part 1 (#82)
    * scene exporter part 1
    
    * actor editing support
    
    * lighting editor
    
    * remove cutscene data copy

--- fast64_internal/hm64/__init__.py
+++ fast64_internal/hm64/__init__.py
@@ -13,6 +13,7 @@ def hm64_register():
     from .z64.skeleton import register as register_z64_skeleton
     from .z64.model_classes_hm64 import register as register_z64_model_classes_hm64
     from .z64.o2r_import import register as register_z64_o2r_import
+    from .z64.scene import register as register_z64_scene
 
     register_f3d_gbi_hm64()
     register_f3d_material_hm64()
@@ -24,6 +25,7 @@ def hm64_register():
     register_z64_panels()
     register_z64_model_classes_hm64()
     register_z64_o2r_import()
+    register_z64_scene()
 
 
 def hm64_unregister():
@@ -37,7 +39,9 @@ def hm64_unregister():
     from .z64.skeleton import unregister as unregister_z64_skeleton
     from .z64.model_classes_hm64 import unregister as unregister_z64_model_classes_hm64
     from .z64.o2r_import import unregister as unregister_z64_o2r_import
+    from .z64.scene import unregister as unregister_z64_scene
 
+    unregister_z64_scene()
     unregister_z64_o2r_import()
     unregister_z64_model_classes_hm64()
     unregister_z64_panels()
